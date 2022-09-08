02e53c478177f06fa8f8e99d203d48a424be6b20
songsurl00 <songsurl@naver.com>
2022-09-08 18:13:38 +0900

    JS : bascis

---
+++ 14_JS_Basics/variables.js
@@ -0,0 +1,12 @@
+let exNum = 5;
+
+exNum += 1; // 숫자 변수 업데이트 (6)
+
+const constNum = 26; // 상수 => 업데이트 할 수 없는 변수
+
+// boolean
+true;
+false;
+
+// _ 대신 대문자로 띄어쓰기를 구분 : Camel case
+const myAge = 23;
