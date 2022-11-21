0166478529cec869d4866eace6ac37a6c89dae17
Sugeun <songsurl@naver.com>
2022-11-21 23:49:23 +0900

    JS : OOP

---
+++ 29_JS_OOP/Prototypes/app.js
@@ -0,0 +1,7 @@
+String.prototype.yell = function () {
+    return `OMG!!! ${this.toUpperCase()}!!! AGHGHGH`;
+};
+
+Array.prototype.pop = function () {
+    return "SORRY I WANT THAT ELEMENT, I WILL NEVER POP IT OFF!"
+}
